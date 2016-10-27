8d56f1e46df6eb819c68b29bf3838d29ab309a24
chishen <chishen1010@qq.com>
2016-10-27 20:52:27 +0800

    change

--- js/common/index-3b5a15bab9.js
+++
@@ -1,1 +0,0 @@
-$(function(){$("#bars").click(function(){$(this).toggleClass("bars-rotate"),$(".navbar").toggleClass("navbar-width")}),$("#bars").hover(function(){$(this).find(".line").css("background","#4c6cb3")},function(){$(this).find(".line").css("background","#fff")})});
